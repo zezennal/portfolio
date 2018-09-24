259f08e75fcad37d76f7db7a79783fa2e3da6bac
nall <zezennal@gmail.com>
2018-09-25 01:03:06 +0700

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");;
 const menuBranding = document.querySelector(".menu-branding");
-const navItem = document.querySelectorAll("nav-item");
+const navItem = document.querySelectorAll(".nav-item");
 
 // Set Initial State Of Menu
 let showMenu = false;
@@ -16,7 +16,7 @@ function toggleMenu() {
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        navItem.forEach(item => item.classList.add('shhow'));
+        navItem.forEach(item => item.classList.add('show'));
     
         // Set Menu State
         showMenu = true;
@@ -26,7 +26,7 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItem.forEach(item => item.classList.remove('shhow'));
+        navItem.forEach(item => item.classList.remove('show'));
     
         // Set Menu State
         showMenu = false;
